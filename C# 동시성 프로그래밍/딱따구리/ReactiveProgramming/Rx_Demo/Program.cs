7c6c619d8cda96da6aa442fb8f068c2f391114ec
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-22 20:27:00 +0900

    feat : add Observable

--- C# 동시성 프로그래밍/딱따구리/ReactiveProgramming/Rx_Demo/Program.cs
+++ C# 동시성 프로그래밍/딱따구리/ReactiveProgramming/Rx_Demo/Program.cs
@@ -1,12 +1,20 @@
 ﻿using Rx_Demo;
+using System.Reactive.Linq;
 
 //var evenNumber = new EvneNumberObservable();
 //var consoleObserver = new ConsoleLogObserver();
 //evenNumber.Subscribe(consoleObserver);
 
-var evenNumber = new EvenNumberSubject();
-evenNumber.Subscribe(Console.WriteLine);
-evenNumber.Run();
+//var evenNumber = new EvenNumberSubject();
+//evenNumber.Run();
+//evenNumber.Subscribe(Console.WriteLine);
+
+Observable.Range(1, 100)
+    .Where(x => x % 2 == 0)
+    .Take(10)
+    .Merge(Observable.Range(1, 100).Where(x => x % 2 == 0).Take(10))
+    .Select(x => $"Even Number {x}")
+    .Subscribe(Console.WriteLine);
 
 Console.WriteLine("Completed!");
 Console.ReadLine();
