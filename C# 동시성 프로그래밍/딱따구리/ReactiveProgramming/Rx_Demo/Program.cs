ed852c652f0f4e083107929a25ecb79f758afa17
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-04-22 20:10:36 +0900

    feat : add EvenNumberSubject

--- C# 동시성 프로그래밍/딱따구리/ReactiveProgramming/Rx_Demo/Program.cs
+++ C# 동시성 프로그래밍/딱따구리/ReactiveProgramming/Rx_Demo/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Rx_Demo;
 
-var evenNumber = new EvneNumberObservable();
-var consoleObserver = new ConsoleLogObserver();
-evenNumber.Subscribe(consoleObserver);
+//var evenNumber = new EvneNumberObservable();
+//var consoleObserver = new ConsoleLogObserver();
+//evenNumber.Subscribe(consoleObserver);
+
+var evenNumber = new EvenNumberSubject();
+evenNumber.Subscribe(Console.WriteLine);
+evenNumber.Run();
 
 Console.WriteLine("Completed!");
 Console.ReadLine();
