88acdddc617c026505dc87d726df211c4ff4aa20
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-05-21 03:38:36 +0900

    feat : add stack queue

---
+++ C# 동시성 프로그래밍/CSharpTest/Immutable_Stack_Queue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+
+ImmutableStack<int> stack = ImmutableStack<int>.Empty;
+
+stack = stack.Push(13);
+
+ImmutableStack<int> biggerStack = stack.Push(7);
+
+foreach (int item in biggerStack)
+    Trace.WriteLine(item); // 7, 13
+
+foreach (int item in stack)
+    Trace.WriteLine(item); // 13 (Snap Shot) (13은 Memory 공유)
+
+int lastItem;
+stack = stack.Pop(out lastItem);
+Trace.WriteLine(lastItem); // 7
+
+
+
+ImmutableQueue<int> queue = ImmutableQueue<int>.Empty;
+
+queue = queue.Enqueue(13);
+
+ImmutableQueue<int> biggerQueue = queue.Enqueue(7);
+
+foreach (int item in biggerQueue)
+    Trace.WriteLine(item); // 13, 7
+
+foreach (int item in queue)
+    Trace.WriteLine(item); // 13 (Snap Shot) (13은 Memory 공유)
+
+int nextItem;
+queue = queue.Dequeue(out nextItem);
+Trace.WriteLine(nextItem); // 13
