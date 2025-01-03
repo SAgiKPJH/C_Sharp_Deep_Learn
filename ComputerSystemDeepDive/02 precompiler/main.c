dec529c84b7fb65bfeb1f5e38be7b7ae4462d4a3
juhyung1021 <juhyung1021@gmail.com>
2025-01-03 07:59:30 +0000

    feat : add main c code precompiler

---
+++ ComputerSystemDeepDive/02 precompiler/main.c
@@ -0,0 +1,10 @@
+// cd "ComputerSystemDeepDive/02 precompiler C"
+// Compile
+// gcc -E main.c > out
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n 1 + 1 = %d, 1>>1 = %d \n", 1+1, 1>>1 );
+    return 0;
+}
