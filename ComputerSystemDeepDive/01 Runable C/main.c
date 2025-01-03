af984ac031e4143a677ce4bc1b3434ed5d5374b7
juhyung1021 <juhyung1021@gmail.com>
2025-01-03 07:34:43 +0000

    feat : add runable c code

---
+++ ComputerSystemDeepDive/01 Runable C/main.c
@@ -0,0 +1,12 @@
+// cd "ComputerSystemDeepDive/01 Runable C"
+// Compile
+// gcc -o example main.c
+// # Run
+// ./example
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
