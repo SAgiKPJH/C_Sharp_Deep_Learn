39310781e25c9cbe960b095b26318f9b746c96f3
juhyung1021 <juhyung1021@gmail.com>
2025-01-03 08:02:45 +0000

    feat : add main c code assembly

---
+++ ComputerSystemDeepDive/03 assembly code/main.c
@@ -0,0 +1,10 @@
+// cd "ComputerSystemDeepDive/03 assembly code"
+// Compile
+// gcc -S main.c > main.s
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n 1 + 1 = %d, 1>>1 = %d \n", 1+1, 1>>1 );
+    return 0;
+}
